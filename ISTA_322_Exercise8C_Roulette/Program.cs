e0f164e057823fc6d57c2b25f166e9cd7bd3deae
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-29 18:26:58 -0400

    Add project files.

---
+++ ISTA_322_Exercise8C_Roulette/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTA_322_Exercise8C_Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
