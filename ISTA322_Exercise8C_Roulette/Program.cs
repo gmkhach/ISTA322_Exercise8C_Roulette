2811e566e8f471a518ec689031282617c6d8cc78
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-29 23:09:02 -0400

    added looping logic to spin the Roulette as many times as desired

--- ISTA322_Exercise8C_Roulette/Program.cs
+++ ISTA322_Exercise8C_Roulette/Program.cs
@@ -10,210 +10,68 @@ namespace ISTA322_Exercise8C_Roulette
     {
         static void Main(string[] args)
         {
-        Run();
+            Run();
         }
 
         private static void Run()
         {
             // 37 represents 00
             int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37 };
-            string[] colors = { "green", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black", "red", "black", "red", "black", "red", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black", "red", "black", "red", "black",  "red", "green" };
+            string[] colors = { "green", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black", "red", "black", "red", "black", "red", "red", "black", "red", "black", "red", "black", "red", "black", "red", "black", "black", "red", "black", "red", "black", "red", "black", "red", "green" };
             RouletteWheel myWheel = new RouletteWheel(numbers);
-            int number = myWheel.Spin();
-            CalcWinningBids(number, colors[number]);
+            bool keepLooping = true;
+            do
+            {
+                int number = myWheel.Spin();
+                CalcWinningBids(number, colors[number]);
+                bool innerLoop = true;
+                do
+                {
+                    try
+                    {
+                        Console.Write("\nDo you want to spin again? Y/N\n\n>>> ");
+                        string input = Console.ReadLine();
+                        if (input == "N" || input == "n")
+                        {
+                            innerLoop = false;
+                            keepLooping = false;
+                        }
+                        else if (input == "Y" || input == "y")
+                        {
+                            innerLoop = false;
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            throw new Exception("\nInvalid entry!");
+                        }
+                        Console.Clear();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (innerLoop);
+            } while (keepLooping);
         }
 
-        private static void CalcWinningBids(int number, string colors)
+        private static void CalcWinningBids(int number, string color)
         {
             string winningBin = Convert.ToString(number);
-            if (number == 37)
-            {
-                winningBin = "00";
-            }
-
-            string evenOdd = "odd";
-            if (winningBin == "00" || number % 2 == 0)
-            {
-                evenOdd = "even";
-            }
-
-            string lowHigh = "high";
-            if (winningBin == "00" || number <= 18)
-            {
-                lowHigh = "low";
-            }
-
-            string dozen = "3rd 12";
-            if (winningBin == "00" || number <= 12)
-            {
-                dozen = "1st 12";
-            }
-            else if (number > 12 && number < 25)
-            {
-                dozen = "2nd 12";
-            }
-
-            string column = "3rd";
-            if (number % 3 == 1)
-            {
-                column = "1st";
-            }
-            else if (number % 3 == 2)
-            {
-                column = "2nd";
-            }
-
-            string street = "";
-            int remainder = number % 3;
-            if (number == 0 || number == 37)
-            {
-                street = $"{winningBin}";
-            }
-            else if (remainder == 0)
-            { 
-                street = $"{number - 2}/{number - 1}/{number}";
-            }
-            else if (remainder == 1 && remainder !=37)
-            {
-                street = $"{number}/{number + 1}/{number + 2}";
-            }
-            else if (remainder == 2)
-            {
-                street = $"{number - 1}/{number}/{number + 1}";
-            }
-
-            string sixNumbers;
-            if (number == 0 || number == 37)
-            {
-                sixNumbers = $"{winningBin}";
-            }
-            else if (number == 34)
-            {
-                sixNumbers = "31/32/33/34/35/36";
-            }
-            else if (remainder == 0 && (number != 1 || number != 34))
-            {
-                sixNumbers = $"{number - 5}/{number - 4}/{number - 3}/{number - 2}/{number - 1}/{number}, {number - 2}/{number - 1}/{number}/{number + 1}/{number + 2}/{number + 3}";
-            }
-            else if (remainder == 1 && number != 37)
-            {
-                sixNumbers = $"{number - 3}/{number - 2}/{number - 1}/{number}/{number + 1}/{number + 2}, {number}/{number + 1}/{number + 2}/{number + 3}/{number + 4}/{number + 5}";
-            }
-            else
-            {
-                sixNumbers = $"{number - 4}/{number - 3}/{number - 2}/{number - 1}/{number}/{number + 1}, {number - 1}/{number}/{number + 1}/{number + 2}/{number + 3}/{number + 4}";
-            }
-
-            string split = "";
-            if (number == 0 || number == 37)
-            {
-                split = $"{winningBin}";
-            }
-            else if (number < 4)
-            {
-                if (remainder == 0)
-                {
-                    split = $"{number - 1}/{number}, {number}/{number + 3}";
-                }
-                else if (remainder == 1 && number != 37)
-                {
-                    split = $"{number}/{number + 1}, {number}/{number + 3}";
-                }
-                else
-                {
-                    split = $"{number - 1}/{number}, {number}/{number + 1}, {number}/{number + 3}";
-                }
-            }
-            else if (number > 33)
-            {
-                if (remainder == 0)
-                {
-                    split = $"{number - 3}/{number}, {number - 1}/{number}";
-                }
-                else if (remainder == 1 && number != 37)
-                {
-                    split = $"{number -3}/{number}, {number}/{number + 1}";
-                }
-                else
-                {
-                    split = $"{number - 3}/{number}, {number -1}/{number}, {number}/{number + 1}";
-                }
-            }
-            else if (remainder == 0)
-            {
-                split = $"{number - 3}/{number}, {number - 1}/{number}, {number}/{number + 3}";
-            }
-            else if (remainder == 1)
-            {
-                split = $"{number - 3}/{number}, {number}/{number + 1}, {number}/{number + 3}";
-            }
-            else
-            {
-                    split = $"{number - 3}/{number}, {number - 1}/{number}, {number}/{number + 1}, {number}/{number + 3}";
-            }
-
-
-            string corner = "";
-            if (number == 0 || number == 37)
-            {
-                corner = $"{winningBin}";
-            }
-            else if (number < 4)
-            {
-                if (remainder == 0)
-                {
-                    corner = $"{number - 1}/{number}/{number + 2}/{number + 3}";
-                }
-                else if (remainder == 1)
-                {
-                    corner = $"{number}/{number + 1}/{number + 3}/{number + 4}";
-                }
-                else
-                {
-                    corner = $"{number - 1}/{number}/{number + 2}/{number + 3}, {number}/{number + 1}/{number + 3}/{number + 4}";
-                }
-            }
-            else if (number > 33)
-            {
-                if (remainder == 0)
-                {
-                    corner = $"{number - 4}/{number - 3}/{number - 1}/{number}";
-                }
-                else if (remainder == 1)
-                {
-                    corner = $"{number - 3}/{number - 2}/{number}/{number + 1}";
-                }
-                else
-                {
-                    corner = $"{number - 4}/{number - 3}/{number - 1}/ {number}, {number - 3}/{number - 2}/{number}/{number + 1}";
-                }
-            }
-            else if (remainder == 0)
-            {
-                corner = $"{number - 4}/{number -3}/{number - 1}/{number}, {number - 1}/{number}/{number + 2}/{number + 3}";
-            }
-            else if (remainder == 1)
-            {
-                corner = $"{number - 3}/{number - 2}/{number}/{number + 1}, {number}/{number + 1}/{number + 3}/{number + 4}";
-            }
-            else
-            {
-                corner = $"{number - 4}/{number - 3}/{number - 1}/ {number}, {number - 3}/{number - 2}/{number}/{number + 1}, {number -1}/{number}/{number + 2}/{number + 3}, {number}/{number + 1}/{number +3}/{number + 4}";
-            }
-
-            Console.WriteLine($"\nWinning bin:\t{winningBin} {colors}");
+            WinningBids bin = new WinningBids(number, color);
+            Console.WriteLine($"\nWinning bin:\t{winningBin} {color}");
             Console.WriteLine("-------------------------");
-            Console.WriteLine($"Numbers:\t{winningBin}");
-            Console.WriteLine($"\nEvens/Odds:\t{evenOdd}");
-            Console.WriteLine($"\nReds/Blacks:\t{colors}");
-            Console.WriteLine($"\nLows/Highs:\t{lowHigh}");
-            Console.WriteLine($"\nDozens:\t\t{dozen}");
-            Console.WriteLine($"\nColumns:\t{column}");
-            Console.WriteLine($"\nStreets:\t{street}");
-            Console.WriteLine($"\n6 Numbers:\t{sixNumbers}");
-            Console.WriteLine($"\nSplit:\t\t{split}");
-            Console.WriteLine($"\nCorner:\t\t{corner}");
-            Console.WriteLine();
+            bin.WinningBin();
+            bin.EvenOdd();
+            bin.RedBlack();
+            bin.LowHigh();
+            bin.Dozens();
+            bin.Columns();
+            bin.Streets();
+            bin.SixNumbers();
+            bin.Split();
+            bin.Corner();
         }
     }
 }
---
+++ ISTA322_Exercise8C_Roulette/WinningBids.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTA322_Exercise8C_Roulette
+{
+    class WinningBids
+    {
+        public WinningBids(int number, string color)
+        {
+            this.n = number;
+            this.remainder = number % 3;
+            this.winningBin = Convert.ToString(number);
+            this.color = color;
+        }
+
+        private int n;
+        private int remainder;
+        private string color;
+        private string winningBin;
+
+        public void WinningBin()
+        {
+            string winningBin = Convert.ToString(n);
+            if (n == 37)
+            {
+                winningBin = "00";
+            }
+            Console.WriteLine($"Numbers:\t{winningBin}");
+        }
+
+        public void EvenOdd()
+        {
+            string evenOdd = "odd";
+            if (winningBin == "00" || n % 2 == 0)
+            {
+                evenOdd = "even";
+            }
+            Console.WriteLine($"\nEvens/Odds:\t{evenOdd}");
+        }
+
+        public void RedBlack()
+        {
+            Console.WriteLine($"\nReds/Blacks:\t{color}");
+        }
+
+        public void LowHigh()
+        {
+            string lowHigh = "high";
+            if (winningBin == "00" || n <= 18)
+            {
+                lowHigh = "low";
+            }
+            Console.WriteLine($"\nLows/Highs:\t{lowHigh}");
+        }
+
+        public void Dozens()
+        {
+            string dozen = "3rd 12";
+            if (winningBin == "00" || n <= 12)
+            {
+                dozen = "1st 12";
+            }
+            else if (n > 12 && n < 25)
+            {
+                dozen = "2nd 12";
+            }
+            Console.WriteLine($"\nDozens:\t\t{dozen}");
+
+        }
+
+        public void Columns()
+        {
+            string column = "3rd";
+            if (n % 3 == 1)
+            {
+                column = "1st";
+            }
+            else if (n % 3 == 2)
+            {
+                column = "2nd";
+            }
+            Console.WriteLine($"\nColumns:\t{column}");
+        }
+
+        public void Streets()
+        {
+            string street = "";
+            if (n == 0 || n == 37)
+            {
+                street = $"{winningBin}";
+            }
+            else if (remainder == 0)
+            {
+                street = $"{n - 2}/{n - 1}/{n}";
+            }
+            else if (remainder == 1 && remainder != 37)
+            {
+                street = $"{n}/{n + 1}/{n + 2}";
+            }
+            else if (remainder == 2)
+            {
+                street = $"{n - 1}/{n}/{n + 1}";
+            }
+
+            Console.WriteLine($"\nStreets:\t{street}");
+        }
+
+        public void SixNumbers()
+        {
+            string sixNumbers;
+            if (n == 0 || n == 37)
+            {
+                sixNumbers = $"{winningBin}";
+            }
+            else if (n == 34)
+            {
+                sixNumbers = "31/32/33/34/35/36";
+            }
+            else if (remainder == 0 && (n != 1 || n != 34))
+            {
+                sixNumbers = $"{n - 5}/{n - 4}/{n - 3}/{n - 2}/{n - 1}/{n}, {n - 2}/{n - 1}/{n}/{n + 1}/{n + 2}/{n + 3}";
+            }
+            else if (remainder == 1 && n != 37)
+            {
+                sixNumbers = $"{n - 3}/{n - 2}/{n - 1}/{n}/{n + 1}/{n + 2}, {n}/{n + 1}/{n + 2}/{n + 3}/{n + 4}/{n + 5}";
+            }
+            else
+            {
+                sixNumbers = $"{n - 4}/{n - 3}/{n - 2}/{n - 1}/{n}/{n + 1}, {n - 1}/{n}/{n + 1}/{n + 2}/{n + 3}/{n + 4}";
+            }
+
+            Console.WriteLine($"\n6 Numbers:\t{sixNumbers}");
+
+        }
+
+        public void Split()
+        {
+            string split = "";
+            if (n == 0 || n == 37)
+            {
+                split = $"{winningBin}";
+            }
+            else if (n < 4)
+            {
+                if (remainder == 0)
+                {
+                    split = $"{n - 1}/{n}, {n}/{n + 3}";
+                }
+                else if (remainder == 1 && n != 37)
+                {
+                    split = $"{n}/{n + 1}, {n}/{n + 3}";
+                }
+                else
+                {
+                    split = $"{n - 1}/{n}, {n}/{n + 1}, {n}/{n + 3}";
+                }
+            }
+            else if (n > 33)
+            {
+                if (remainder == 0)
+                {
+                    split = $"{n - 3}/{n}, {n - 1}/{n}";
+                }
+                else if (remainder == 1 && n != 37)
+                {
+                    split = $"{n - 3}/{n}, {n}/{n + 1}";
+                }
+                else
+                {
+                    split = $"{n - 3}/{n}, {n - 1}/{n}, {n}/{n + 1}";
+                }
+            }
+            else if (remainder == 0)
+            {
+                split = $"{n - 3}/{n}, {n - 1}/{n}, {n}/{n + 3}";
+            }
+            else if (remainder == 1)
+            {
+                split = $"{n - 3}/{n}, {n}/{n + 1}, {n}/{n + 3}";
+            }
+            else
+            {
+                split = $"{n - 3}/{n}, {n - 1}/{n}, {n}/{n + 1}, {n}/{n + 3}";
+            }
+            Console.WriteLine($"\nSplit:\t\t{split}");
+        }
+
+        public void Corner()
+        {
+            string corner = "";
+            if (n == 0 || n == 37)
+            {
+                corner = $"{winningBin}";
+            }
+            else if (n < 4)
+            {
+                if (remainder == 0)
+                {
+                    corner = $"{n - 1}/{n}/{n + 2}/{n + 3}";
+                }
+                else if (remainder == 1)
+                {
+                    corner = $"{n}/{n + 1}/{n + 3}/{n + 4}";
+                }
+                else
+                {
+                    corner = $"{n - 1}/{n}/{n + 2}/{n + 3}, {n}/{n + 1}/{n + 3}/{n + 4}";
+                }
+            }
+            else if (n > 33)
+            {
+                if (remainder == 0)
+                {
+                    corner = $"{n - 4}/{n - 3}/{n - 1}/{n}";
+                }
+                else if (remainder == 1)
+                {
+                    corner = $"{n - 3}/{n - 2}/{n}/{n + 1}";
+                }
+                else
+                {
+                    corner = $"{n - 4}/{n - 3}/{n - 1}/ {n}, {n - 3}/{n - 2}/{n}/{n + 1}";
+                }
+            }
+            else if (remainder == 0)
+            {
+                corner = $"{n - 4}/{n - 3}/{n - 1}/{n}, {n - 1}/{n}/{n + 2}/{n + 3}";
+            }
+            else if (remainder == 1)
+            {
+                corner = $"{n - 3}/{n - 2}/{n}/{n + 1}, {n}/{n + 1}/{n + 3}/{n + 4}";
+            }
+            else
+            {
+                corner = $"{n - 4}/{n - 3}/{n - 1}/ {n}, {n - 3}/{n - 2}/{n}/{n + 1}, {n - 1}/{n}/{n + 2}/{n + 3}, {n}/{n + 1}/{n + 3}/{n + 4}";
+            }
+            Console.WriteLine($"\nCorner:\t\t{corner}");
+        }
+
+    }
+}
